9ba00871dbdd41439130d23347f82ec4c321fac4
boburtojiboev <tbobur1995@gmail.com>
2023-10-11 20:52:51 +0900

    feat: Task I

---
+++ train.js
@@ -0,0 +1,25 @@
+// 📌  I-Task: 
+// arrayning ichidagi 0 index qiymatni
+//  arrayning oxiriga qoyib return qilsin
+
+const array = ["JS", "Bobur", "Simon", ["1995", "01", "27"], "apple"];
+
+let arr = array.slice(1).concat(array.shift());
+console.log(arr);
+
+
+
+
+
+// let a = array.shift();
+// console.log(a)
+// let b = array.slice();
+// console.log(b);
+// let new_array = b.concat(a)
+
+
+
+
+
+
+
