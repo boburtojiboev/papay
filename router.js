b36973a243b961d146ae4ba21312617a47ac5d4a
boburtojiboev <tbobur1995@gmail.com>
2023-10-13 20:11:47 +0900

    feat: member controllerlarni qurish

--- router.js
+++ router.js
@@ -1,10 +1,14 @@
 const express = require("express");
 const router = express.Router();
+const memberController = require("./controllers/memberController");
 
-router.get("/", function (req, res) {
-    res.send("Home sahifadasiz");
-});
+//  memberga dahldor routerlar
+router.get("/", memberController.home);
+router.post("/signup", memberController.signup);
+router.post("/login", memberController.login);
+router.get("/logout", memberController.logout);
 
+// boshqa routerlar
 router.get("/menu", (req, res) => {
     res.send("Menu sahifadasiz");
 });
