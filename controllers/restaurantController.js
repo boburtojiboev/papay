c50e89d74ebbc0249ee18d253b9e4e52c900d89b
boburtojiboev <tbobur1995@gmail.com>
2023-10-21 11:05:01 +0900

    feat: develop session authentication on ejs

--- controllers/restaurantController.js
+++ controllers/restaurantController.js
@@ -2,6 +2,19 @@ const Member = require("../models/Member");
 
 let restaurantController = module.exports;
 
+restaurantController.getMyRestaurantData= async (req, res) => {
+    try {
+        console.log("GET: cont/getMyRestaurantData");
+     // todo get my res product
+
+     res.render("restaurant-menu");
+    } catch(err) {
+        console.log(`ERROR, cont/getMyRestaurantData, ${err.message}`);
+        res.json({state: 'fail', message: err.message});
+    }
+};
+
+
 restaurantController.getSignupMyRestaurant = async (req, res) => {
     try {
         console.log("GET: cont/getSignUpMyRestautant");
@@ -11,8 +24,6 @@ restaurantController.getSignupMyRestaurant = async (req, res) => {
         res.json({state: 'fail', message: err.message});
     }
 };
-
-
  
 restaurantController.signupProcess = async (req, res) => {
     try {
@@ -23,7 +34,9 @@ restaurantController.signupProcess = async (req, res) => {
         const member = new Member(),
         new_member = await member.signupData(data);
 
-        res.json({state: 'succeed', data: new_member});
+        req.session.member = new_member;
+        res.redirect("/resto/pruducts/menu");
+        
     } catch(err){
         console.log(`ERROR, cont/signup, ${err.message}`);
         res.json({state: 'fail', message: err.message}); 
@@ -49,17 +62,26 @@ restaurantController.loginProcess = async (req, res) => {
         member = new Member(),
         result = await member.loginData(data);
 
-        // SESSION
-
-        res.json({state: 'succeed', data: result});
+        req.session.member = result;
+        req.session.save(function() {
+            res.redirect("/resto/pruducts/menu");
+        });
     } catch(err){
         console.log(`ERROR, cont/login, ${err.message}`);
         res.json({state: 'fail', message: err.message}); 
     }
 };
-
+ 
 
 restaurantController.logout = (req, res) => {
     console.log("GET cont/logout");
     res.send("Logout sahifadasiz");
-};
+}; 
+
+restaurantController.checkSessions = (req, res) => {
+    if (req.session?.member){
+        res.json({state: 'succeed', data: req.session.member});
+    } else {
+      res.json({state: 'fail', message: "You are not authenticated"});  
+    }
+};  
