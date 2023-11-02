51184e51084224985da3de18aab40c310c62294b
boburtojiboev <tbobur1995@gmail.com>
2023-11-02 20:53:28 +0900

    feat: start new product creation logic

--- controllers/productController.js
+++ controllers/productController.js
@@ -30,7 +30,7 @@ productController.addNewProduct = async (req, res) => {
     const result = await product.addNewProductData(data, req.member); 
 
     const html = `<script>
-                   alert(new dish added succesfully);
+                   alert(new product added succesfully);
                    window.location.replace('/resto/products/menu');
                    </script>`;
     res.end(html);      
