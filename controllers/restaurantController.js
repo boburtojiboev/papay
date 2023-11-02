5f14846ae0d3364304d263b64eb304718adf7f27
boburtojiboev <tbobur1995@gmail.com>
2023-11-02 17:41:52 +0900

    fix: modify logout business logic

--- controllers/restaurantController.js
+++ controllers/restaurantController.js
@@ -99,8 +99,16 @@ restaurantController.loginProcess = async (req, res) => {
 };
 
 restaurantController.logout = (req, res) => {
-  console.log("GET cont/logout"); 
-  res.send("Logout sahifadasiz");
+  try {
+    console.log("GET: cont/logout");
+    req.session.destroy(function() {
+      res.redirect("/resto");
+    });
+    
+  } catch (err) {
+    console.log(`ERROR, cont/logout, ${err.message}`);
+    res.json({ state: "fail", message: err.message });
+  }
 };
 
 restaurantController.validateAuthRestaurant = (req, res, next) => {
