58ef631f4a6e85df6f5a639a2b31fea97075cf29
boburtojiboev <tbobur1995@gmail.com>
2023-11-29 20:59:54 +0900

    feat: create jwt tokens and enable cookie-parser

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 const router = require("./router");
 const router_bssr = require("./router_bssr")
-// const router_bssr = require("./router_bssr");
+const cookieParser = require("cookie-parser");
 
 let session = require("express-session");
 const MongoDBStore = require("connect-mongodb-session")(session); 
@@ -22,6 +22,7 @@ const store =  new MongoDBStore({
 app.use(express.static("public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
+app.use(cookieParser());
 
 // 2: session code 
 app.use(
--- controllers/memberController.js
+++ controllers/memberController.js
@@ -1,42 +1,73 @@
 const Member = require("../models/Member");
 let memberController = module.exports;
- 
+const jwt = require("jsonwebtoken");
+const assert = require("assert");
+const Definer = require("../lib/mistake");
+
 memberController.signup = async (req, res) => {
-    try {
-        console.log("POST: cont/signup");
-        const data = req.body;
-        console.log("body:::", req.body);
+  try {
+    console.log("POST: cont/signup");
+    const data = req.body;
+    console.log("body:::", req.body);
+    const member = new Member(),
+      new_member = await member.signupData(data);
 
-        const member = new Member(),
-        new_member = await member.signupData(data);
+        // token hosil qilinyabdi
+      const token = memberController.createToken(new_member);
+      res.cookie("access_token", token, {
+        maxAge: 6 * 3600 * 1000,
+        httpOnly: true,
+      });
 
-        // TODO:  AUTHENTICATE BESED ON JWT
-
-        res.json({state: 'succeed', data: new_member});
-    } catch(err){
-        console.log(`ERROR, cont/signup, ${err.message}`);
-        res.json({state: 'fail', message: err.message}); 
-    }
+    res.json({ state: "succeed", data: new_member });
+  } catch (err) {
+    console.log(`ERROR, cont/signup, ${err.message}`);
+    res.json({ state: "fail", message: err.message });
+  }
 };
- 
 
 memberController.login = async (req, res) => {
-    try {
-        console.log("POST: cont/login");
-        const data = req.body;
-        // console.log("body:::", req.body);
-        member = new Member(),
-        result = await member.loginData(data);
+  try {
+    console.log("POST: cont/login");
+    const data = req.body;
+    // console.log("body:::", req.body);
+    member = new Member();
+    result = await member.loginData(data);
 
-        res.json({state: 'succeed', data: result});
-    } catch(err){
-        console.log(`ERROR, cont/login, ${err.message}`);
-        res.json({state: 'fail', message: err.message}); 
-    }
+    // token hosil qilinyabdi
+    const token = memberController.createToken(result);
+    res.cookie("access_token", token, {
+      maxAge: 6 * 3600 * 1000,
+      httpOnly: true,
+    });
+
+    res.json({ state: "succeed", data: result });
+  } catch (err) {
+    console.log(`ERROR, cont/login, ${err.message}`);
+    res.json({ state: "fail", message: err.message });
+  }
 };
 
-
 memberController.logout = (req, res) => {
-    console.log("GET cont/logout");
-    res.send("Logout sahifadasiz");
+  console.log("GET cont/logout");
+  res.send("Logout sahifadasiz");
+};
+
+memberController.createToken = (result) => {
+  try {
+    const upload_date = {
+      _id: result._id,
+      mb_nick: result.mb_nick,
+      mb_type: result.mb_type,
+    };
+
+    const token = jwt.sign(upload_date, process.env.SECRET_TOKEN, {
+      expiresIn: "6h",
+    });
+
+    assert.ok(token, Definer.auth_err2);
+    return token;
+  } catch (err) {
+    throw err;
+  }
 };
