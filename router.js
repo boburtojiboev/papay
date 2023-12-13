45c8f36a9caac32bc49a1a04d4e86fd56ade0ecd
boburtojiboev <tbobur1995@gmail.com>
2023-12-13 23:07:25 +0900

    feat: develop buseness logic of member subscribe

--- router.js
+++ router.js
@@ -5,6 +5,7 @@ const productController = require("./controllers/productController");
 const restaurantController = require("./controllers/restaurantController");
 const orderController = require("./controllers/orderController.js");
 const communityController = require("./controllers/communityController.js");
+const followController = require("./controllers/followController.js");
 const uploader_community = require("./utils/upload-multer.js")("community");
 const uploader_member = require("./utils/upload-multer.js")("members");
 
@@ -94,6 +95,13 @@ router.get(
   "/community/single-article/:art_id",
   memberController.retrieveAuthMember,
   communityController.getChosenArticles
+);
+
+// Follow related routers
+router.post(
+  "/follow/subscribe",
+  memberController.retrieveAuthMember,
+  followController.subscribe
 );
 
 module.exports = router;
