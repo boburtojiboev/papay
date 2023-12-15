1e0d3fd6d9f61130ef3225214795417f90067741
boburtojiboev <tbobur1995@gmail.com>
2023-12-15 23:29:07 +0900

    feat: develop business logic of member unsubscribe rest api

--- controllers/followController.js
+++ controllers/followController.js
@@ -1,19 +1,35 @@
-let follorController = module.exports;
+let followController = module.exports;
 const assert = require("assert");
 const Definer = require("../lib/mistake");
 const Follow = require("../models/Follow");
 
-follorController.subscribe = async (req, res) => {
+followController.subscribe = async (req, res) => {
   try {
     console.log("POST: cont/subscribe");
     assert.ok(req.member, Definer.auth_err5);
 
     const follow = new Follow();
-    await follow.subscribeData(req.member, req.body)
+    await follow.subscribeData(req.member, req.body);
 
     res.json({ state: "success", data: "subscribed" });
   } catch (err) {
     console.log(`ERROR, cont/subscribe, ${err.message}`);
+    res.json({ state: "fail", message: err.message });
+  }
+};
+
+followController.unsubscribe = async (req, res) => {
+  try {
+    console.log("POST, cont/unsubscribe");
+    assert.ok(req.member, Definer.auth_err5);
+
+    const follow = new Follow();
+    await follow.unsubscribeData(req.member, req.body);
+    assert.ok(result, Definer.general_err1);
+
+    res.json({ state: "success", data: "unsubscribed" });
+  } catch (err) {
+    console.log(`ERROR, cont/unsubscribe`);
     res.json({ state: "fail", message: err.message });
   }
 };
