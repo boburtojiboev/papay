dd3045b28c26b57cfbb31549592402d56a08acdb
boburtojiboev <tbobur1995@gmail.com>
2024-01-11 17:17:06 +0900

    feat: develop updateMember rest api business logic

--- router.js
+++ router.js
@@ -27,6 +27,12 @@ router.post(
   memberController.retrieveAuthMember,
   memberController.likeMemberChosen
 );
+router.post(
+  "/member/update",
+  memberController.retrieveAuthMember,
+  uploader_member.single("mb_image"),
+  memberController.updateMember
+);
 
 // Product related routers
 router.post(
